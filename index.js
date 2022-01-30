07916106644d1781cefaafcaf35ea49330f8ed83
shankuroko <shanakarsar9942@gmail.com>
2022-01-30 17:10:57 +0530

    test-1

--- index.js
+++ index.js
@@ -1,29 +1,128 @@
-const express = require('express'); //Import the express dependency
-const app = express();  
+const express = require('express');
+const app = express();
 var cors = require('cors');
-          //Instantiate an express app, the main work horse of this server
-const port = process.env.PORT||5000;                  //Save the port number where your server will be listening
-app.use(cors()); 
+const port = process.env.PORT || 5000;
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-//Idiomatic expression in express to route and respond to a client request
-app.get('/', (req, res) => {        //get requests to the root ("/") will route here
-    //res.sendFile('index.html', {root: __dirname});      //server responds by sending the index.html file to the client's browser
-   // console.log(req.params);
-
-   res.setHeader('Content-Type', 'text/html');
-   res.type('html'); 
-   res.end(`<script>console.log(${req.query.a})</script>`);
-    //res.write(`hello ${req.params['a']}`);                                                //the .sendFile method needs the absolute path to the file, see: https://expressjs.com/en/4x/api.html#res.sendFile 
+var food = [{
+    name: "",
+    price: 0,
+    img: "",
+    qty: 1
+}, {
+    name: "",
+    price: 0,
+    img: "",
+    qty: 1
+}, {
+    name: "",
+    price: 0,
+    img: "",
+    qty: 1
+}, {
+    name: "",
+    price: 0,
+    img: "",
+    qty: 1
+}, {
+    name: "",
+    price: 0,
+    img: "",
+    qty: 1
+}, {
+    name: "",
+    price: 0,
+    img: "",
+    qty: 1
+}, {
+    name: "",
+    price: 0,
+    img: "",
+    qty: 1
+}];
+var drinks = [{
+    name: "",
+    price: 0,
+    img: "",
+    qty: 1
+}, {
+    name: "",
+    price: 0,
+    img: "",
+    qty: 1
+}, {
+    name: "",
+    price: 0,
+    img: "",
+    qty: 1
+}, {
+    name: "",
+    price: 0,
+    img: "",
+    qty: 1
+}, {
+    name: "",
+    price: 0,
+    img: "",
+    qty: 1
+}];
+var cart = {
+    data: []
+};
+app.get('/', (req, res) => {
+    res.setHeader('Content-Type', 'text/html');
+    res.type('html');
+    let out = '';
+    if (req.query.item == 'food') {
+        for (let i = 0; i < food.length; i++) {
+            out += `<div class="menu-item"><div class="menu-item_container">`;
+            out += `<i>${food[i].name}</i>`;
+            out += `<i>${food[i].price}</i>`;
+            out += `<div class="menu-item-btn" onclick="addToCart(${JSON.stringify(food[i])})">Add Item</div></div></div>`;
+        }
+    } else if (req.query.item == 'drinks') {
+        for (let i = 0; i < drinks.length; i++) {
+            out += `<div class="menu-item"><div class="menu-item_container">`;
+            out += `<i>${drinks[i].name}</i>`;
+            out += `<i>${drinks[i].price}</i>`;
+            out += `<div class="menu-item-btn" onclick="addToCart(${JSON.stringify(drinks[i])})">Add Item</div></div></div>`;
+        }
+    }
+    res.end(out);
 });
-app.post('/',(req, res) => {        //get requests to the root ("/") will route here
-    //res.sendFile('index.html', {root: __dirname});      //server responds by sending the index.html file to the client's browser
-   // console.log(req.params);
+app.get('/food', (req, res) => {
+    res.setHeader('Content-Type', 'text/html');
+    res.type('html');
+    let out = '';
+    for (let i = 0; i < food.length; i++) {
+        out += `<div class="menu-item"><div class="menu-item_container">`;
+        out += `<i>${food[i].name}</i>`;
+        out += `<i>${food[i].price}</i>`;
+        out += `<div class="menu-item-btn" onclick="addToCart(${JSON.stringify(food[i])})">Add Item</div></div></div>`;
+    }
+    res.end(out);
 
-   res.setHeader('Content-Type', 'text/html');
-   res.type('html'); 
-   res.end(`<script>console.log(${req.body.a})</script>`);
 });
-app.listen(port, () => {            //server starts listening for any attempts from a client to connect at port: {port}
-    console.log(`Now listening on port ${port}`); 
+
+app.get('/drink', (req, res) => {
+    res.setHeader('Content-Type', 'text/html');
+    res.type('html');
+    let out = '';
+    for (let i = 0; i < drinks.length; i++) {
+        out += `<div class="menu-item"><div class="menu-item_container">`;
+        out += `<i>${drinks[i].name}</i>`;
+        out += `<i>${drinks[i].price}</i>`;
+        out += `<div class="menu-item-btn" onclick="addToCart(${JSON.stringify(drinks[i])})">Add Item</div></div></div>`;
+    }
+    res.end(out);
+});
+app.post('/add', (req, res) => {
+
+    res.setHeader('Content-Type', 'text/html');
+    res.type('html');
+    res.end(`<script>console.log(${req.body.a})</script>`);
+});
+app.listen(port, () => { //server starts listening for any attempts from a client to connect at port: {port}
+    console.log(`Now listening on port ${port}`);
 });
--- text.js
+++ text.js
@@ -1,4 +1,14 @@
 const express = require('express');
-var url = require('url');
-const test=url('localhost:5000/',true);
-console.log(url);
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.get('/', (req, res) => {
+    let p = document.createElement('p');
+    p.innerText = "Hello";
+    res.setHeader('Content-Type', 'text/html');
+    res.type('html');
+    res.end(p);
+});
+app.listen(port, () => { //server starts listening for any attempts from a client to connect at port: {port}
+    console.log(`Now listening on port ${port}`);
+});
