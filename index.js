4d9659e8f649f8bdaefe226107fc373732971f20
shankuroko <shanakarsar9942@gmail.com>
2022-01-30 12:04:10 +0530

    authentication

--- index.js
+++ index.js
@@ -6,7 +6,10 @@ const port = process.env.PORT||5000;                  //Save the port number whe
 app.get('/', (req, res) => {        //get requests to the root ("/") will route here
     //res.sendFile('index.html', {root: __dirname});      //server responds by sending the index.html file to the client's browser
    // console.log(req.params);
-    res.send('done');
+
+   res.setHeader('Content-Type', 'application/json');
+   res.type('json'); 
+   res.end(JSON.stringify({ a: 1 }));
     //res.write(`hello ${req.params['a']}`);                                                //the .sendFile method needs the absolute path to the file, see: https://expressjs.com/en/4x/api.html#res.sendFile 
 });
 
