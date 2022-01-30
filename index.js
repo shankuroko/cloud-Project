b7616e5d17b38d49b84565e3dd965374c37f3171
shankuroko <shanakarsar9942@gmail.com>
2022-01-30 11:51:26 +0530

    authentication

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ const port = process.env.PORT||5000;                  //Save the port number whe
 //Idiomatic expression in express to route and respond to a client request
 app.get('/', (req, res) => {        //get requests to the root ("/") will route here
     //res.sendFile('index.html', {root: __dirname});      //server responds by sending the index.html file to the client's browser
-    console.log(req.params);
-    res.write('done');
+   // console.log(req.params);
+    res.send('done');
     //res.write(`hello ${req.params['a']}`);                                                //the .sendFile method needs the absolute path to the file, see: https://expressjs.com/en/4x/api.html#res.sendFile 
 });
 
