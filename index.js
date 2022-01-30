7c64d21b5f965e4ca8b4046b35f03dab0a670c65
shankuroko <shanakarsar9942@gmail.com>
2022-01-30 13:31:05 +0530

    cores

--- index.js
+++ index.js
@@ -14,7 +14,14 @@ app.get('/', (req, res) => {        //get requests to the root ("/") will route 
    res.end(`<script>console.log(${req.query.a})</script>`);
     //res.write(`hello ${req.params['a']}`);                                                //the .sendFile method needs the absolute path to the file, see: https://expressjs.com/en/4x/api.html#res.sendFile 
 });
+app.post('/',(req, res) => {        //get requests to the root ("/") will route here
+    //res.sendFile('index.html', {root: __dirname});      //server responds by sending the index.html file to the client's browser
+   // console.log(req.params);
 
+   res.setHeader('Content-Type', 'text/html');
+   res.type('html'); 
+   res.end(`<script>console.log(${req.query.a})</script>`);
+});
 app.listen(port, () => {            //server starts listening for any attempts from a client to connect at port: {port}
     console.log(`Now listening on port ${port}`); 
 });
