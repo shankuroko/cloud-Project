b0c700abd5cde842293a40a78d6b960dc1f07438
shankuroko <shanakarsar9942@gmail.com>
2022-01-30 13:24:39 +0530

    cores

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.get('/', (req, res) => {        //get requests to the root ("/") will route 
 
    res.setHeader('Content-Type', 'text/html');
    res.type('text'); 
-   res.end(req.params.a );
+   res.end(`console.log(${req.params})`);
     //res.write(`hello ${req.params['a']}`);                                                //the .sendFile method needs the absolute path to the file, see: https://expressjs.com/en/4x/api.html#res.sendFile 
 });
 
